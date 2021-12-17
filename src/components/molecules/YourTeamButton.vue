093ac3742fdb06ea966edbac0a4b2b7a0e3f01cd
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-12-17 08:59:43 +0700

    Fisnish up your team button

--- src/components/molecules/YourTeamButton.vue
+++ src/components/molecules/YourTeamButton.vue
@@ -10,14 +10,14 @@ const props: YourTeamButtonProps = withDefaults(defineProps<YourTeamButtonProps>
 <template>
     <button class="w-[180px] h-[40px] bg-bump-your-team-btn rounded-[10px]">
         <div class="flex flex-row">
-            <div class="pl-[10px]">
-                <img src="../../assets/Your_Team_icon.svg" class="inline-block align-middle"/>
-            </div>
-            <div class="pl-[10px]">
-                <span class="text-[14px] font-medium text-bumpp-blue-txt inline-block align-middle">
+            <!-- <div class="pl-[10px] inline-block align-middle"> -->
+                <img src="../../assets/Your_Team_icon.svg" class="pl-[10px]"/>
+            <!-- </div> -->
+            <!-- <div class="pl-[10px] inline-block align-middle"> -->
+                <span class="text-[14px] pl-[10px] font-medium text-bumpp-blue-txt ">
                     {{props.btnText}}
                 </span>
-            </div>
+            <!-- </div> -->
             </div>
     </button>
 </template>
